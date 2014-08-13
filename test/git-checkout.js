021194e4f321704bcb03a436cf22d9347bace56c
Matthew Chase Whittemore <matthew@socialtables.com>
2014-08-13 00:57:41 -0400

    updating the Hookfile to track commits by day

---
+++ test/git-checkout.js
@@ -0,0 +1,24 @@
+describe("git-checkout, when GHM is in use,", function(){
+
+	beforeEach(function(done){
+		TEST_SUITE.setup(true, true, true, done);
+	});
+
+	describe("when checking out a branch", function(){
+
+		beforeEach(function(done){
+
+		});
+
+		it("should run", function(done){
+
+		});
+	});
+
+	describe("when checking out a file", function(){
+		it("should run", function(done){
+
+		});
+	});
+
+});
