571db110ada2bf3a78121c21d4c90a001986df91
Matthew Chase Whittemore <matthew@socialtables.com>
2014-08-17 14:00:50 -0400

    checkout tests are done. close #7

--- test/git-checkout.js
+++ test/git-checkout.js
@@ -1,23 +1,61 @@
-describe("git-checkout, when GHM is in use,", function(){
+describe.only("git-checkout, when GHM is in use,", function(){
 
 	beforeEach(function(done){
-		TEST_SUITE.setup(true, true, true, done);
+		TEST_SUITE.setup(true, true, true, function(err){
+			if(err){
+				done(err);
+			}
+			else{
+				TEST_SUITE.git.__makeAddAndCommitFile("test", "test", done);
+			}
+		});
 	});
 
 	describe("when checking out a branch", function(){
 
-		beforeEach(function(done){
-
-		});
-
 		it("should run", function(done){
-
+			TEST_SUITE.git.checkout("-b test", function(err, stdout, stderr){
+				if(err){
+					done(err);
+				}
+				else{
+					try{
+						stderr.should.equal("Switched to a new branch 'test'\npost-checkout\n\n");
+						done();
+					}
+					catch(err){
+						done(err);
+					}
+				}
+			});
 		});
 	});
 
 	describe("when checking out a file", function(){
-		it("should run", function(done){
 
+		it("should reset to old content if the file has changed", function(done){
+			TEST_SUITE.mkfile("test", "hmmmm", function(err){
+				if(err){
+					done(err);
+				}
+				else{
+					TEST_SUITE.git.checkout("-- test", function(err, stdout, stderr){
+						if(err){
+							done(err);
+						}
+						else{
+							try{
+								stderr.should.equal("post-checkout\n\n");
+								TEST_SUITE.getFileContents("test").should.equal("test\n");
+								done();
+							}
+							catch(err){
+								done(err);
+							}
+						}
+					});
+				}
+			});
 		});
 	});
 
