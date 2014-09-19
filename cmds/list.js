5e2452ae552da22fca9beed31da7da7ebe8cf5c3
Matthew Chase Whittemore <matthew@socialtables.com>
2014-09-19 01:35:13 -0400

    simpler list with numbers

--- cmds/list.js
+++ cmds/list.js
@@ -1,34 +1,11 @@
-var exec = require("child_process").exec;
 var parseHookfile = require("../lib/parse-hookfile");
 
 module.exports = function(hook, args){
 	var parsed = parseHookfile(process.cwd());
 	var commands = parsed[hook] || [];
-	listCommands(commands, 0);
-}
 
-function listCommands(commands, i){
-	if(i<commands.length){
-		var cmd = commands[i];
-		listCommand(cmd, function(err){
-			if(err){
-				throw err;
-			}
-			else{
-				listCommands(commands, i+1);
-			}
-		});
+	for(var i=0; i<commands.length; i++){
+		console.log(i+")", commands[i]);
 	}
-}
 
-function listCommand(cmd, cb){
-	exec("echo "+cmd, function(err, stdout){
-		if(err){
-			cb(err);
-		}
-		else{
-			console.log(stdout);
-			cb();
-		}
-	});
 }
